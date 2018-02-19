caca721be5ea847a3d040ff66704077d510f3711
Nam-H-Nguyen <nguyenbydoing@gmail.com>
2018-02-19 14:04:06 -0800

    Add Calculator stack view constraints

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by NomNomNam on 2/19/18.
+//  Copyright © 2018 NamHNguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var calcNumLabel: UILabel!
+    @IBAction func calcButtonPressed(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
