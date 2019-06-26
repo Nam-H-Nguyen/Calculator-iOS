6146f68d507b5ec8eb977f39fe0ea2c6ce74c6cd
Nam-H-Nguyen <nguyenbydoing@gmail.com>
2019-06-26 16:42:25 -0700

    Finish Calculator expression app

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -7,11 +7,117 @@
 //
 
 import UIKit
+import Expression
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var calcNumLabel: UILabel!
+    
+    var sbResult = ""
+    
     @IBAction func calcButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func acBtn(_ sender: Any) {
+        sbResult = ""
+        calcNumLabel.text = ""
+    }
+    
+    @IBAction func leftParenBtn(_ sender: Any) {
+        sbResult += "("
+        calcNumLabel.text = "\(sbResult)"
+    }
+    
+    @IBAction func rightParenBtn(_ sender: Any) {
+        sbResult += ")"
+        calcNumLabel.text = "\(sbResult)"
+    }
+    
+    @IBAction func divBtn(_ sender: Any) {
+        sbResult += "/"
+        calcNumLabel.text = "\(sbResult)"
+    }
+    
+    @IBAction func multBtn(_ sender: Any) {
+        sbResult += "*"
+        calcNumLabel.text = "\(sbResult)"
+    }
+    
+    @IBAction func subBtn(_ sender: Any) {
+        sbResult += "-"
+        calcNumLabel.text = "\(sbResult)"
+    }
+    
+    @IBAction func addBtn(_ sender: Any) {
+        sbResult += "+"
+        calcNumLabel.text = "\(sbResult)"
+    }
+    
+    @IBAction func equalBtn(_ sender: Any) {
+        let expression = Expression(sbResult)
+        do {
+            let result = try expression.evaluate()
+            calcNumLabel.text = "\(result)"
+
+            print("Result: \(result)")
+        } catch {
+            print("Error: \(error)")
+        }
+    }
+    
+    @IBAction func decBtn(_ sender: Any) {
+        sbResult += "."
+        calcNumLabel.text = "\(sbResult)"
+    }
+    
+    @IBAction func zeroBtn(_ sender: Any) {
+        sbResult += "0"
+        calcNumLabel.text = "\(sbResult)"
+    }
+    
+    @IBAction func oneBtn(_ sender: Any) {
+        sbResult += "1"
+        calcNumLabel.text = "\(sbResult)"
+    }
+    
+    @IBAction func twoBtn(_ sender: Any) {
+        sbResult += "2"
+        calcNumLabel.text = "\(sbResult)"
+    }
+    
+    @IBAction func threeBtn(_ sender: Any) {
+        sbResult += "3"
+        calcNumLabel.text = "\(sbResult)"
+    }
+    
+    @IBAction func fourBtn(_ sender: Any) {
+        sbResult += "4"
+        calcNumLabel.text = "\(sbResult)"
+    }
+    
+    @IBAction func fiveBtn(_ sender: Any) {
+        sbResult += "5"
+        calcNumLabel.text = "\(sbResult)"
+    }
+    
+    @IBAction func sixBtn(_ sender: Any) {
+        sbResult += "6"
+        calcNumLabel.text = "\(sbResult)"
+    }
+    
+    @IBAction func sevenBtn(_ sender: Any) {
+        sbResult += "7"
+        calcNumLabel.text = "\(sbResult)"
+    }
+    
+    @IBAction func eightBtn(_ sender: Any) {
+        sbResult += "8"
+        calcNumLabel.text = "\(sbResult)"
+    }
+    
+    @IBAction func nineBtn(_ sender: Any) {
+        sbResult += "9"
+        calcNumLabel.text = "\(sbResult)"
     }
     
     override func viewDidLoad() {
